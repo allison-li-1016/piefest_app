b8ab4f266af839c16498bc5c7513bb2a01f52f11
allisonli <allli@uw.edu>
2025-03-01 21:08:19 -0800

    adding backend

--- piefest_backend/app.js
+++ piefest_backend/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const multer = require('multer');
 
-const LOCAL_PORT = 3000;
+const LOCAL_PORT = 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
