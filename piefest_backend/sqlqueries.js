50ae0943cd94d115f78b044ba9eb5776c0398f97
benjamin <benjamin.brusniak@gmail.com>
2025-03-23 15:56:16 -0700

    Add admin call to setup db

---
+++ piefest_backend/sqlqueries.js
@@ -0,0 +1,49 @@
+const CreateUserTableQuery = 
+`
+CREATE TABLE Users (
+    UserId INT IDENTITY(1,1) PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(64) NOT NULL
+);
+`
+const CreateAdminTableQuery =
+`
+CREATE TABLE Admins (
+    AdminId INT IDENTITY(1,1) PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL UNIQUE,
+    Password VARCHAR(64) NOT NULL
+);
+`
+const CheckAdminCredentialsQuery =
+`
+SELECT AdminId 
+FROM Admins 
+WHERE Username = @user
+AND Password = @pass;
+`
+const CreatePieTableQuery =
+`
+Create TABLE Pies (
+    PieId INT IDENTITY(1,1) PRIMARY KEY,
+    PieName VARCHAR(100) NOT NULL
+);
+`
+const CreateVotesTableQuery =
+`
+CREATE TABLE Votes (
+    UserId INT NOT NULL,
+    PieId INT NOT NULL,
+    Vote FLOAT NOT NULL CHECK (Vote >= 0 AND Vote <= 10),
+    PRIMARY KEY (UserID, PieID),
+    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+    FOREIGN Key (PieID) REFERENCES Pies(PieID)
+);
+`
+
+module.exports = {
+    CreateUserTableQuery,
+    CreateAdminTableQuery,
+    CreatePieTableQuery,
+    CreateVotesTableQuery,
+    CheckAdminCredentialsQuery
+}
