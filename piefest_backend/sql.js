47db1d3fb6b43e94af1d7a49e73b453c8a81d29c
allisonli <allli@uw.edu>
2025-03-23 16:37:24 -0700

    moving backend logic to the app class

--- piefest_backend/sql.js
+++ piefest_backend/sql.js
@@ -47,33 +47,6 @@ async function ConnectAndQuery(sqlQuery, paramMap = new Map()) {
     return resultSet.recordset;
 }
 
-async function VoteForPie(pieId, vote, userId) {
-    try {
-        if (!Number.isInteger(userId)) {
-            throw new Error("Invalid userId: must be an integer.");
-        }
-        if (!Number.isInteger(pieId)) {
-            throw new Error("Invalid pieId: must be an integer.");
-        }
-        if (typeof vote !== "number" || Number.isInteger(vote)) {
-            throw new Error("Invalid vote: must be a float.");
-        }
-        if (vote < 0 || vote > 10) {
-            throw new Error("Vote value must be between 0 and 10.");
-        }
-
-        await ConnectAndQuery(VoteForPieQuery, new Map([
-            ['userID', userId],
-            ['pieID', pieId], 
-            ['vote', vote]
-        ]));
-
-        console.log("Vote casted successfully.");
-    } catch (err) {
-        console.error(`Vote cast failed: ${err.message}`);
-    }
-}
-
 module.exports = {
     ConnectAndQuery
 }
